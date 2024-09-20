b26ca043c6c4c5ecd8f85536ebd5e7133bda4352
splmloic <l2ct@protonmail.com>
2024-09-20 16:29:12 +0200

    auth completed, that was TRICKY

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,34 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 @auth.route("/login", methods=['GET','POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        email =request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('logged in success !', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('incorrect password, please try again', category='error')
+        else:
+            flash('Email does not exist', category='error')
+    return render_template('login.html', user=current_user)
 
 @auth.route("/logout")
+@login_required
 def logout():
-    return "<p>logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route("/sign-up", methods=['GET','POST'])
 def sign_up():
@@ -16,13 +36,16 @@ def sign_up():
         data = request.form
         print(data)
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password = request.form.get('password')
         confirm_password = request.form.get('confirm-password')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email aleady used.', category='error')
+        elif len(email) < 4:
             flash('Email must be greater than 4 characters.', category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('First name must be greater than 2 characters.', category='error')
         elif password != confirm_password:
             flash('Your passwords dont match', category='error')
@@ -30,7 +53,11 @@ def sign_up():
             flash('Your password must be greater than 6 characters', category='error')
         else:
             # add user to database
-            print('account created')
+            new_user = User(email=email,first_name=first_name,password=generate_password_hash(password, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created!', category='success')
+            login_user(user, remember=True)
+            return redirect(url_for('views.home'))
 
-    return render_template('signup.html')
+    return render_template('signup.html',user=current_user)
