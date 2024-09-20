0587817e08fb8fd3ce84a61e5dc944b1616417aa
splmloic <l2ct@protonmail.com>
2024-09-20 10:42:12 +0200

    Much better template with dedicated routes/private route file

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-from flask import Flask, render_template
+from website import create_app
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template('index.html')
+app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(debug=True)
